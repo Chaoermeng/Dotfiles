d004ba126140fc474cbd1d7103da81c14078d1df
Chaoermeng <zhangchao050530@gmail.com>
2025-11-30 02:11:45 +0800

    feat(yazi/themes.toml): add themes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -45,3 +45,13 @@ abbr td "tailscale down"
 abbr tp "tailscale ping"
 abbr ts "tailscale status"
 abbr tu "tailscale up"
+
+# Functions
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
